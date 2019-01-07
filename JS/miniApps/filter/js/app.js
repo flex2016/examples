23aef2713250ee901b1c0abd8090afbf5c50d0d7
flex2016 <reloded2004@hotmail.com>
2019-01-07 13:53:30 -0500

    added modal

--- JS/miniApps/filter/js/app.js
+++ JS/miniApps/filter/js/app.js
@@ -30,7 +30,6 @@
 
 
 //search input
-
 (function(){
   //target search box
   const search = document.getElementById('search-item');
@@ -50,4 +49,70 @@
       }
     });
   });
+})();
+
+
+//lightbox
+(function(){
+  //all images
+  let imageList = [];
+  let counter = 0;
+
+  const images = document.querySelectorAll('.store-img');
+  const container = document.querySelector('.lightbox-container');
+  const item = document.querySelector('.lightbox-item');
+  const closeIcon = document.querySelector('.lightbox-close');
+  const btnLeft = document.querySelector('.btnLeft');
+  const btnRight = document.querySelector('.btnRight');
+
+  //add all images
+  images.forEach(function(img){
+    imageList.push(img.src);
+  });
+
+  //open modal
+  images.forEach(function(img){
+    img.addEventListener('click', function(event){
+      //show modal
+      container.classList.add('show');
+      //get source
+      let src = event.target.src;
+      //get index of the image source
+      counter = imageList.indexOf(src);
+      //show modal with an image
+      item.style.backgroundImage = `url(${src})`;
+    });
+  });
+
+  //close modal
+  closeIcon.addEventListener('click', function(){
+    container.classList.remove('show');
+  });
+
+  //left button
+  btnLeft.addEventListener('click', function(){
+    counter--;
+    if (counter < 0){
+      counter = imageList.length -1;
+    }
+    item.style.backgroundImage = `url(${imageList[counter]})`;
+    console.log(counter);
+
+  });
+
+  //left button
+  btnRight.addEventListener('click', function(){
+    counter++;
+    if (counter > imageList.length - 1){
+      counter = 0;
+    }
+    item.style.backgroundImage = `url(${imageList[counter]})`;
+    console.log(counter);
+
+  });
+
+
+
+
+
 })();
