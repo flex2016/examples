da7c97584a83e9acd3e969aaf3621a8b48bfd13f
flex2016 <reloded2004@hotmail.com>
2019-02-27 17:37:27 -0500

    setup few controllers

---
+++ JS/miniApps/budgetMVC/app.js
@@ -0,0 +1,94 @@
+
+//BUDGET CONTROLLER
+var budgetController = (function(){
+
+  var Expense = function(id, description, value){
+    this.id =id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value){
+    this.id =id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var allExpenses = [];
+  var allIncomes = [];
+
+  var data = {
+    allItems:{
+      exp : [],
+      inc : []
+    },
+    totals: {
+      exp : 0,
+      inc : 0
+    }
+  }
+
+})();
+
+
+//UI CONTROLLER
+var UIController= (function(){
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription : '.add__description',
+    inputValue : '.add__value',
+    inputBtn : '.add__btn'
+  };
+
+  return {
+    getInput : function(){
+      return {
+        type : document.querySelector(DOMstrings.inputType).value,
+        description : document.querySelector(DOMstrings.inputDescription).value,
+        value : document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings : function(){
+      return DOMstrings;
+    }
+  };
+
+})();
+
+
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+  var setupEventListeners = function(){
+    var DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+      if(event.keyCode === 13 || event.which === 13){
+        ctrlAddItem();
+
+      }
+    });
+  };
+
+  var ctrlAddItem = function(){
+    //  Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
+
+  };
+
+  return {
+    init :function(){
+      console.log('application has started');
+      setupEventListeners();
+    }
+  };
+
+})(budgetController, UIController);
+
+controller.init();
