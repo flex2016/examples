a39e1318f11f2229555436bb6dd4caa374d7143b
flex2016 <reloded2004@hotmail.com>
2019-04-07 12:12:15 -0400

    added get products class

--- JS/miniApps/ShoppingCartComfyHouse/app.js
+++ JS/miniApps/ShoppingCartComfyHouse/app.js
@@ -14,10 +14,30 @@ const ProductsDOM = document.querySelector(".products-center");
 let cart = [];
 
 //getting the products
-class Products {}
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.items;
+      //destructure teh data
+      products = products.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 //display products
-class UI {}
+class UI {
+  displayProducts(products) {}
+}
 
 //local storage
 class Storage {}
@@ -25,4 +45,7 @@ class Storage {}
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
+
+  // get all products
+  products.getProducts().then(data => console.log(data));
 });
