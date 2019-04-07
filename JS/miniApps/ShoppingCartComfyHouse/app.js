48fdd7a4002c95fd150b7fb617bd0780f1b9b40c
flex2016 <reloded2004@hotmail.com>
2019-04-07 10:39:24 -0400

    initial app js setup

---
+++ JS/miniApps/ShoppingCartComfyHouse/app.js
@@ -0,0 +1,28 @@
+//variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const ProductsDOM = document.querySelector(".products-center");
+
+//main cart
+let cart = [];
+
+//getting the products
+class Products {}
+
+//display products
+class UI {}
+
+//local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+});
