8465a9659466b5d86b2d146a324feec0fb49e403
flex2016 <reloded2004@hotmail.com>
2019-04-08 15:22:44 -0400

    added local storage and cart methods

--- JS/miniApps/ShoppingCartComfyHouse/app.js
+++ JS/miniApps/ShoppingCartComfyHouse/app.js
@@ -12,6 +12,8 @@ const productsDOM = document.querySelector(".products-center");
 
 //main cart
 let cart = [];
+//buttons
+let buttonsDOM = [];
 
 //getting the products
 class Products {
@@ -61,15 +63,92 @@ class UI {
     });
     productsDOM.innerHTML = result;
   }
+  getBagButtons() {
+    //change button nodelist to an array
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = buttons;
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+      if (inCart) {
+        button.innerText = "In Cart";
+        button.disabled = true;
+      }
+      button.addEventListener("click", event => {
+        event.target.innerText = "In Cart";
+        event.target.disabled = true;
+        //get product from products
+        let cartItem = { ...Storage.getProduct(id), amount: 1 };
+        //add product to the cart
+        cart = [...cart, cartItem];
+        //save cart in local storage
+        Storage.saveCart(cart);
+        //set cart values
+        this.setCartValues(cart);
+        //display cart item
+        this.addCartItem(cartItem);
+        //show cart
+      });
+    });
+  }
+  setCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+    // console.log(cartTotal, cartItems);
+  }
+  addCartItem(item) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = `
+      <img src="${item.image}" alt="product" />
+      <div>
+        <h4>${item.title}</h4>
+        <h5>$${item.price}</h5>
+        <span class="remove-item" data-id=${item.id}>remove</span>
+      </div>
+      <div>
+        <div class="fas fa-chevron-up" data-id=${item.id}></div>
+        <p class="item-amount">${item.amount}</p>
+        <div class="fas fa-chevron-down" data-id=${item.id}></div>
+      </div>
+    `;
+    cartContent.appendChild(div);
+    console.log(cartContent);
+  }
 }
 
 //local storage
-class Storage {}
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find(product => product.id === id);
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
 
   // get all products
-  products.getProducts().then(products => ui.displayProducts(products));
+  products
+    .getProducts()
+    .then(products => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getBagButtons();
+    });
 });
