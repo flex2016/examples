ea45fa643534a0aebda76be5d2c5b72f7b4688be
flex2016 <reloded2004@hotmail.com>
2019-04-18 16:28:04 -0400

    react todo v2

---
+++ react/apps/todoapp2/src/components/AddTodo.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class AddTodo extends Component {
+  state = {
+    title: ""
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.addTodo(this.state.title);
+    this.setState({ title: "" });
+  };
+  onChange = e => this.setState({ title: e.target.value });
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Add Todo ..."
+          style={{ flex: "10", padding: "5px" }}
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+        <input
+          type="submit"
+          value="Submit"
+          className="btn"
+          style={{ flex: "1" }}
+        />
+      </form>
+    );
+  }
+}
+
+// PropTypes
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired
+};
+export default AddTodo;
