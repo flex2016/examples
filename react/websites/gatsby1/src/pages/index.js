28cca222392c7b01cb2ccd981dbec8e6290cfd6b
flex2016 <reloded2004@hotmail.com>
2019-06-13 14:42:48 -0400

    added card section

--- react/websites/gatsby1/src/pages/index.js
+++ react/websites/gatsby1/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Link from "gatsby-link"
 import Layout from "../components/layout"
+import Card from "../components/Card"
 
 const IndexPage = () => (
   <Layout>
@@ -45,6 +46,31 @@ const IndexPage = () => (
               />
             </path>
           </svg>
+        </div>
+      </div>
+      <div className="Cards">
+        <h2>11 courses, more coming</h2>
+        <div className="CardGroup">
+          <Card
+            title="Design System"
+            text="10 sections"
+            image={require("../images/wallpaper.jpg")}
+          />
+          <Card
+            title="React for Designers"
+            text="12 sections"
+            image={require("../images/wallpaper2.jpg")}
+          />
+          <Card
+            title="Sound Design"
+            text="5 sections"
+            image={require("../images/wallpaper3.jpg")}
+          />
+          <Card
+            title="ARKit 2"
+            text="10 sections"
+            image={require("../images/wallpaper4.jpg")}
+          />
         </div>
       </div>
     </div>
