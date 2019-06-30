761a8ee0f37e6cc494628b08ed3330abd1ddce35
flex2016 <reloded2004@hotmail.com>
2019-06-30 19:46:10 -0400

    added click event and moved image location

--- react/apps/city-tours/src/components/Tour/Tour.js
+++ react/apps/city-tours/src/components/Tour/Tour.js
@@ -2,33 +2,35 @@ import React, { Component } from "react";
 import "./Tour.scss";
 
 export default class Tour extends Component {
+  state = {
+    showInfo: false
+  };
+
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo
+    });
+  };
   render() {
+    const { city, img, name, info } = this.props.tour;
     return (
       <article className="tour">
         <div className="img-container">
-          <img
-            src="https://www-tc.pbs.org/wnet/nature/files/2017/10/reqzNZB-asset-mezzanine-16x9-dWu5cfN.jpg"
-            alt=""
-          />
+          <img src={img} alt="" />
           <span className="close-btn">
             <i className="fas fa-window-close" />
           </span>
         </div>
         <div className="tour-info">
-          <h3>Hello</h3>
-          <h4>name</h4>
+          <h3>{city}</h3>
+          <h4>{name}</h4>
           <h5>
             info
-            <span>
+            <span onClick={this.handleInfo}>
               <i className="fas fa-caret-square-down" />
             </span>
           </h5>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est enim
-            voluptates culpa neque facere aliquid deserunt illum exercitationem
-            quidem recusandae! Eveniet eos dignissimos sit vitae, deserunt sequi
-            animi est modi.
-          </p>
+          {this.state.showInfo && <p>{info}</p>}
         </div>
       </article>
     );
