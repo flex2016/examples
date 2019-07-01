e58713dee8e3061e8aa81e51e83d072f21e83fd9
flex2016 <reloded2004@hotmail.com>
2019-06-30 20:05:34 -0400

    added remove tour function

--- react/apps/city-tours/src/components/TourList/TourList.js
+++ react/apps/city-tours/src/components/TourList/TourList.js
@@ -7,13 +7,22 @@ export default class TourList extends Component {
   state = {
     tours: tourData
   };
+  removeTour = id => {
+    const { tours } = this.state;
+    const sortedTours = tours.filter(tour => tour.id !== id);
+    this.setState({
+      tours: sortedTours
+    });
+  };
   render() {
     const { tours } = this.state;
 
     return (
       <section className="tourlist">
         {tours.map(tour => {
-          return <Tour key={tour.id} tour={tour} />;
+          return (
+            <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+          );
         })}
       </section>
     );
