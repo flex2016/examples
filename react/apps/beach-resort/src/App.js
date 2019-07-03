313526279ee88350d2721b6d0d2d096f33466007
flex2016 <reloded2004@hotmail.com>
2019-07-03 13:59:44 -0400

    added navbar

--- react/apps/beach-resort/src/App.js
+++ react/apps/beach-resort/src/App.js
@@ -8,12 +8,17 @@ import Error from "./pages/Error";
 
 import { Route, Switch } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms" component={Rooms} />
-      <Route exact path="/single-room" component={SingleRoom} />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
