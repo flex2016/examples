e4cbb6f02f245d1b89c914563ac5aba4c2c33805
flex2016 <reloded2004@hotmail.com>
2019-07-03 21:43:07 -0400

    added services section

--- react/apps/beach-resort/src/pages/Error.js
+++ react/apps/beach-resort/src/pages/Error.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
-export default class Error extends Component {
-  render() {
-    return <div>Hello from error page</div>;
-  }
+export default function Error() {
+  return (
+    <Hero>
+      <Banner title="404" subtitle="page not found">
+        <Link to="/" className="btn-primary">
+          Return Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
--- react/apps/beach-resort/src/pages/Rooms.js
+++ react/apps/beach-resort/src/pages/Rooms.js
@@ -1,5 +1,16 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function Rooms() {
-  return <div>Hello from rooms page</div>;
+  return (
+    <Hero className="roomsHero">
+      <Banner title="Our Rooms">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
